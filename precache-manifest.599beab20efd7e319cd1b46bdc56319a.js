9e93446ac26c9f0e3b37282c9f1e89203866bdeb
Greg Schuman <gcschuman@hotmail.com>
2020-10-10 17:22:30 -0700

    Updates

---
+++ precache-manifest.599beab20efd7e319cd1b46bdc56319a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5e89598fa581ac62fb83a43eae1b0b",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "dda876c3a3fc50338b47",
+    "url": "/quote-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0efa63ebfa50b370217",
+    "url": "/quote-generator/static/js/2.a54e18e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.a54e18e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda876c3a3fc50338b47",
+    "url": "/quote-generator/static/js/main.8349d354.chunk.js"
+  },
+  {
+    "revision": "95f6f4bf04c9c9655887",
+    "url": "/quote-generator/static/js/runtime-main.f5556377.js"
+  }
+]);
